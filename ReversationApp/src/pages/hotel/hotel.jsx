dd8c4bf813e607477f508fefb99808bd66ff734d
Arshad Ahmed <arshadahmed10x@gmail.com>
2023-06-02 01:48:25 +0530

    done

--- ReversationApp/src/pages/hotel/hotel.jsx
+++ ReversationApp/src/pages/hotel/hotel.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./hotel.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const hotel = () => {
-  return <div>hotel</div>;
+  return (
+    <>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Elton St 125 New york</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default hotel;
